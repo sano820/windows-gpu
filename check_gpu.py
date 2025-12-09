cb9c56cad563b628f57084a2ba0483eba8954bdf
sano820 <sano820@naver.com>
2025-12-09 14:58:36 +0900

    1209

--- check_gpu.py
+++ check_gpu.py
@@ -5,3 +5,11 @@ print("CUDA 사용 가능?", torch.cuda.is_available())
 print("GPU 개수:", torch.cuda.device_count())
 if torch.cuda.is_available():
     print("0번 GPU 이름:", torch.cuda.get_device_name(0))
+
+import tensorflow as tf
+print("====")
+print("TensorFlow:", tf.__version__)
+print("GPU 리스트:", tf.config.list_physical_devices('GPU'))
+
+print("CUDA로 빌드됨?", tf.test.is_built_with_cuda())
+print("GPU 사용 가능?", tf.test.is_gpu_available())
